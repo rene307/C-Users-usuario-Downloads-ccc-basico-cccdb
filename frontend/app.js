15a167304467e5b8179f17362ef3ad3375e8c64d
rene307 <109702609+rene307@users.noreply.github.com>
2026-08-20 21:07:26 -0400

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
-const API_BASE = "http://localhost:3000/api";
-//const API_BASE = "/api";
+//const API_BASE = "http://localhost:3000/api";
+const API_BASE = "/api";
 
 let token =
   sessionStorage.getItem("ccc_token") || "";
